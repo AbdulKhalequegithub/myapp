9ae2af2055fb24b630e6953e18575481d10d878b
AbdulKhalequegithub <121848812+AbdulKhalequegithub@users.noreply.github.com>
2023-02-24 21:30:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with the help of Manish Sir 
         </a>
       </header>
     </div>
